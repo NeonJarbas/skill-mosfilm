5d96fd5b3d6e50f7e7e88affa42ef20fd51063c7
jarbasai <jarbasai@mailfence.com>
2023-01-06 14:40:08 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from ovos_utils.parse import fuzzy_match
 from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
     ocp_search, ocp_featured_media
 from youtube_archivist import YoutubeMonitor
-
+import random
 
 class MosfilmSkill(OVOSCommonPlaybackSkill):
     def __init__(self):
@@ -23,12 +23,14 @@ class MosfilmSkill(OVOSCommonPlaybackSkill):
                                                           "soundtrack", " OST", "opening theme"])
 
     def initialize(self):
-        url = "https://www.youtube.com/channel/UCm5U4zqpahzyNXBv5ZT51Jw"
         bootstrap = "https://github.com/JarbasSkills/skill-mosfilm/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
-        self.archive.monitor(url)
-        self.archive.setDaemon(True)
-        self.archive.start()
+        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+
+    def _sync_db(self):
+        url = "https://www.youtube.com/channel/UCm5U4zqpahzyNXBv5ZT51Jw"
+        self.archive.parse_videos(url)
+        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
 
     # matching
     def match_skill(self, phrase, media_type):
